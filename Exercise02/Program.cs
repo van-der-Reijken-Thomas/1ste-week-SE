f43084a36250979fbed5873361147a1bdf693bdb
van-der-Reijken-Thomas <thomas.van.der.reijken@student.howest.be>
2026-09-25 14:29:01 +0200

    Complete exercise 2 calculation

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("waarde A");
+string waardeA = Console.ReadLine();
+
+Console.WriteLine("waarde B");
+string waardeB = Console.ReadLine();
+
+int a = int.Parse(waardeA);
+int b = int.Parse(waardeB);
+
+int som = a + b;
+Console.WriteLine($"De som van {a} en {b} is {som}.");
